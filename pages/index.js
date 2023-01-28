0538e705f6ddee7fcf9141d6f510be7652d56077
Kashan Haider <kashan@Kashans-MacBook-Air.local>
2023-01-29 01:11:47 +0530

    Changes in index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="https://drive.google.com/file/d/1fcLjCAE0lspTfFRYOlzTENiBNjv9ZT8F/view?usp=sharing" target='_blank'
                 >
-                  Download Resume
+                  Resume
                 </a>
               </li>
               
